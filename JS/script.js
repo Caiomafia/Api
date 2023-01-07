21c8e1fe61441ec2d085316caf9328f05c74ee92
Caio Mafra <gamersb200@gmail.com>
2023-01-07 11:56:57 -0300

    Novo projeto

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#form');
+form.addEventListener('submit' , handleSubmit);
+
+let status = document.getElementById('status');
+let  attempt = document.getElementById('attemp');
+let result = document.getElementById('result');
+
+const 
+
+
+
+function handleSubmit(e){}
+
+function playAgain(){
+    document.getElementById('btnRestart').style.display = 'flex';
+
+}
+
+function restar(){
+    document.location.reload(true)
+}
+
+
+function clear(){
+    document.querySelector('#kick').value = '';
+}
